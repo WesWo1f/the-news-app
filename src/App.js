c21a9f841fac1a4aefc1d0fb554f673f6937a418
Wes <claude.w.fountain@gmail.com>
2023-01-17 18:33:49 -0500

    trying to fix api call

--- src/App.js
+++ src/App.js
@@ -8,39 +8,62 @@ import { useEffect, useState } from 'react';
 
 function App() {
 
-  const [newsData, setNewsData] = useState()
-  useEffect(()=>{
-    console.log(newsData)
-  },[newsData])
-
-
-  const wolf = 'wolf'
-  useEffect(()=>{
-    async function requestData(id) {
-        console.log("Asked server for id: "+id)
-      try {
-        const response = await fetch('https://nameless-cliffs-00097.herokuapp.com/category', {
-          method: 'POST', // or 'PUT'
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({category: "wolf"})
-      })
-       .then((response) => response.json())
-       .then((data) => console.log(data));
-      // setNewsData(await response.json())
-      // console.log('made call to server')
-      } catch (error) {
-        console.log("it ran but server is not on")
-        console.log(error)
-      }
-    }
-  requestData(wolf)
-  },[])
-  useEffect(()=>{
-    console.log(newsData)
+  // const [newsData, setNewsData] = useState()
+  // useEffect(()=>{
+  //   console.log(newsData)
+  // },[newsData])
+  useEffect(()=> {
+   async function requestData() {
+       console.log("Asked server for id: "+id)
+     try {
+       const response = await fetch('https://nameless-cliffs-00097.herokuapp.com', {
+         method: 'GET', // or 'PUT'
+         headers: {
+           'Accept': 'application/json',
+           'Content-Type': 'application/json',
+         },
+     })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+     // setNewsData(await response.json())
+      console.log('made call to server')
+     } catch (error) {
+       console.log("it ran but server is not on")
+       console.log(error)
+     }
+   }
+   requestData()
   })
+
+
+  // const wolf = 'wolf'
+  // useEffect(()=>{
+  //   async function requestData(id) {
+  //       console.log("Asked server for id: "+id)
+  //     try {
+  //       const response = await fetch('https://nameless-cliffs-00097.herokuapp.com/category', {
+  //         method: 'POST', // or 'PUT'
+  //         headers: {
+  //           'Accept': 'application/json',
+  //           'Content-Type': 'application/json',
+  //         },
+  //         body: JSON.stringify({category: "wolf"})
+  //     })
+  //      .then((response) => response.json())
+  //      .then((data) => console.log(data));
+  //     // setNewsData(await response.json())
+  //     // console.log('made call to server')
+  //     } catch (error) {
+  //       console.log("it ran but server is not on")
+  //       console.log(error)
+  //     }
+  //   }
+  // requestData(wolf)
+  // },[])
+
+  // useEffect(()=>{
+  //   console.log(newsData)
+  // })
 
 
 
