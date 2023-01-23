a5a434c95304a17178d2f23138ec32d69bf9e463
Wes <claude.w.fountain@gmail.com>
2023-01-23 15:38:37 -0500

    testing server fetch call

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import NewsNavbar from './components/NewsNavbar';
 import { useRef } from 'react'
 import { useEffect, useState } from 'react';
 
-//https://nameless-cliffs-00097.herokuapp.com/
-//http://localhost:8000/category
 
 function App() {
 
@@ -28,35 +26,6 @@ useEffect(()=>{
     });
 })
 
-useEffect(()=>{
-  console.log("local host server was called")
-  fetch('http://localhost:8000/category', {
-    method: 'POST', // or 'PUT'
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log('Success:', data);
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
-})
-
-
-    
-
-
-
-
-
-
-
-
-
 
 
 
@@ -70,33 +39,3 @@ useEffect(()=>{
 }
 
 export default App;
-
-
-
-// export default function NewsFetch({ searchId, setNewsData}) {
-
-
-//     useEffect(()=>{
-//             async function requestData(id) {
-//                 console.log("Asked server for id: "+id)
-//               try {
-//                 const response = await fetch('http://localhost:8000/category', {
-//                   method: 'POST', // or 'PUT'
-//                   headers: {
-//                     'Accept': 'application/json',
-//                     'Content-Type': 'application/json',
-//                   },
-//                   body: JSON.stringify({category:id})
-//               })
-//               setNewsData(await response.json())
-//               console.log('made call to server')
-//               } catch (error) {
-//                 console.log("it ran but server is not on")
-//                 console.log(error)
-//               }
-//             }
-//         requestData(searchId)
-//     },[searchId])
-
-//     return (null);
-// }
