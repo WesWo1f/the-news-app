ab9a816b503588f8ad6f310c858d4e577d3e77d2
Wes <claude.w.fountain@gmail.com>
2023-01-28 17:37:28 -0500

    fixed NewsNavbar categorys to directly match api

--- src/components/NewsNavbar.js
+++ src/components/NewsNavbar.js
@@ -4,6 +4,9 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
+
+
+
 export default function NewsNavbar(setSearchId) {
   return (
     <>
@@ -13,11 +16,15 @@ export default function NewsNavbar(setSearchId) {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
+            <Link to="/newsPage/general" className="nav-link">General</Link>
+            <Link to="/newsPage/science" className="nav-link">Science</Link>
+            <Link to="/newsPage/health" className="nav-link">Health</Link>
+            <Link to="/newsPage/tech" className="nav-link">Tech</Link>
             <Link to="/newsPage/sports" className="nav-link">Sports</Link>
-            <Link to="/newsPage/world" className="nav-link">World</Link>
+            <Link to="/newsPage/travel" className="nav-link">travel</Link>
             <Link to="/newsPage/entertainment" className="nav-link">Entertainment</Link>
             <Link to="/newsPage/politics" className="nav-link">Politics</Link>
-            <Link to="/newsPage/us" className="nav-link">U.S.</Link>
+            <Link to="/newsPage/food" className="nav-link">food</Link>
             <Link to="/newsPage/business" className="nav-link">Business</Link>
           </Nav>
         </Navbar.Collapse>
