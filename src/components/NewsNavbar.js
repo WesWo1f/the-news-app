1b5dfd036955fb576a642be5f6c4392f28c4d2bc
Wes <claude.w.fountain@gmail.com>
2023-01-30 18:29:10 -0500

    light styling changing font for navbar and color for newsCrawl

--- src/components/NewsNavbar.js
+++ src/components/NewsNavbar.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import '../styles/navbar.css'
 
 
 
