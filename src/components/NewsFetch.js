9a67fde49779cd670ef0549009c760d8a9d89115
Wes <claude.w.fountain@gmail.com>
2023-01-30 15:31:00 -0500

    added NewsCrawl.js and newsCrawl.css

--- src/components/NewsFetch.js
+++ src/components/NewsFetch.js
@@ -1,10 +1,10 @@
 import { useEffect } from 'react'
 
-export default function NewsFetch({ searchId, setNewsData}) {
+export default function NewsFetch({ searchId, setNewsData, crawlData, setCrawlData}) {
 
    
 
-    useEffect(()=>{
+    useEffect(() => {
             async function requestData(id) {
                 console.log("Asked server for id: "+id)
               try {
@@ -24,7 +24,48 @@ export default function NewsFetch({ searchId, setNewsData}) {
               }
             }
         requestData(searchId)
-    }, [searchId])
+  }, [searchId])
+
+  useEffect(() => {
+
+         async function requestCrawlData(id) {
+           console.log("Asked server for id: "+id)
+         try {
+           const response = await fetch('https://nameless-cliffs-00097.herokuapp.com/crawldata', {
+             method: 'GET', // or 'PUT'
+             headers: {
+               'Accept': 'application/json',
+               'Content-Type': 'application/json',
+             },
+            //body: JSON.stringify({category:id})
+         })
+         setCrawlData(await response.json())
+         console.log('made call to server for crawlData')
+         } catch (error) {
+           console.log("it ran but server is not on")
+           console.log(error)
+         }
+       }
+      //requestCrawlData(searchId)
+
+      if(typeof(crawlData) !== 'undefined'){
+
+        //console.log("this is crawlData: "+crawlData.fetchResult.data[0].title)
+        //newsData.fetchResult.data[0].url
+        //newsData.fetchResult.data[0].uuid
+        //newsData.fetchResult.data[0].language: "en"
+        //newsData.fetchResult.data[0].source
+        //newsData.fetchResult.data[0].keywords
+        //newsData.fetchResult.data[0].categories: (2) ['general', 'politics']
+        //newsData.fetchResult.data[0].image_url
+        //newsData.fetchResult.data[0].description
+      }
+      else{
+        requestCrawlData(searchId)
+        console.log("well this is else")
+      }
+  },)
+
 
 
   return (null);
