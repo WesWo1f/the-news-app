95f5d345bd3a221a691f8747f3912a185b668510
Wes <claude.w.fountain@gmail.com>
2023-02-09 17:31:22 -0500

    refactored newsFetch.js shorter and cleaner

--- src/components/NewsFetch.js
+++ src/components/NewsFetch.js
@@ -1,54 +1,48 @@
 import { useEffect } from 'react'
 
-export default function NewsFetch({ searchId,setRawData, setNewsData, crawlData, setCrawlData}) {
+export default function NewsFetch({searchId, setRawData, crawlData, setCrawlData}) {
 
-   
+  async function fetchData(url, id, page) {
+    let data = {}
+    try {
+      const response = await fetch(url, {
+        method: 'POST', // or 'PUT'
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({category: id, page: page})
+    })
+    data = await response.json()
+    } catch (error) {
+      //console.log(error)
+    }
+    return data
+  }
 
-    useEffect(() => {
-            async function requestData(id) {
-              try {
-                const response = await fetch('https://nameless-cliffs-00097.herokuapp.com/category', {
-                  method: 'POST', // or 'PUT'
-                  headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                  },
-                  body: JSON.stringify({category:id})
-              })
-              setRawData(await response.json())
-              //setNewsData(await response.json())
-              } catch (error) {
-                //console.log(error)
-              }
-            }
-        requestData(searchId)
-  }, [searchId])
-
-  useEffect(() => {
-
-         async function requestCrawlData(id) {
-         try {
-           const response = await fetch('https://nameless-cliffs-00097.herokuapp.com/crawldata', {
-             method: 'GET', // or 'PUT'
-             headers: {
-               'Accept': 'application/json',
-               'Content-Type': 'application/json',
-             },
-            //body: JSON.stringify({category:id})
-         })
-         setCrawlData(await response.json())
-         } catch (error) {
-           //console.log(error)
-         }
-       }
-      if(typeof(crawlData) !== 'undefined'){
-
+  useEffect(()=>{
+    async function combineData(url){
+      const pageOneData = await fetchData(url, searchId, 1)
+      const pageTwoData = await fetchData(url, searchId, 2)
+      //this combines page one and page two
+      const multipageData = pageTwoData.fetchResult.data.concat(pageOneData.fetchResult.data)
+      //the takes the pages(array) from both fetches and combines them into one object
+      pageOneData.fetchResult.data = multipageData
+      //this is just renaming that object to make more sense
+      const combinedDataPages = pageOneData
+      //sets raw data to now be combinedDataPages
+      if(url ==='https://nameless-cliffs-00097.herokuapp.com/category'){
+        setRawData(combinedDataPages)
       }
-      else{
-        requestCrawlData(searchId)
+      else if(url ==='https://nameless-cliffs-00097.herokuapp.com/crawldata'){
+        setCrawlData(combinedDataPages)
       }
-  },)
-
+    }
+    combineData('https://nameless-cliffs-00097.herokuapp.com/category')
+    if(typeof(crawlData) === 'undefined'){
+      combineData('https://nameless-cliffs-00097.herokuapp.com/crawldata')
+    }
+  },[searchId])
 
 
   return (null);
