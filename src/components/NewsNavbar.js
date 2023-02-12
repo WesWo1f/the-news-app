485854ed3c1a1fc9b897aa4f0083f08cddbc23b2
Wes <claude.w.fountain@gmail.com>
2023-02-12 12:11:40 -0500

    styling overhaul

--- src/components/NewsNavbar.js
+++ src/components/NewsNavbar.js
@@ -11,26 +11,31 @@ import '../styles/navbar.css'
 export default function NewsNavbar(setSearchId) {
   return (
     <>
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand ><Link to="/" className="nav-link">News</Link></Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Link to="/newsPage/general" className="nav-link">General</Link>
-            <Link to="/newsPage/science" className="nav-link">Science</Link>
-            {/* <Link to="/newsPage/health" className="nav-link">Health</Link> */}
-            <Link to="/newsPage/tech" className="nav-link">Tech</Link>
-            <Link to="/newsPage/sports" className="nav-link">Sports</Link>
-            <Link to="/newsPage/travel" className="nav-link">Travel</Link>
-            <Link to="/newsPage/entertainment" className="nav-link">Entertainment</Link>
-            <Link to="/newsPage/politics" className="nav-link">Politics</Link>
-            <Link to="/newsPage/food" className="nav-link">Food</Link>
-            <Link to="/newsPage/business" className="nav-link">Business</Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div className='navbar-container'>
+      <div className='brand-and-links'>
+        <Navbar.Brand><Link className='nav-brand' to="/">News</Link></Navbar.Brand>
+      </div>
+      <div className='brand-and-links'>
+        <Navbar   expand="lg">
+          <Container>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse  id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Link to="/newsPage/general" className="nav-link">General</Link>
+                <Link to="/newsPage/science" className="nav-link">Science</Link>
+                <Link to="/newsPage/tech" className="nav-link">Tech</Link>
+                <Link to="/newsPage/sports" className="nav-link">Sports</Link>
+                <Link to="/newsPage/travel" className="nav-link">Travel</Link>
+                <Link to="/newsPage/entertainment" className="nav-link">Entertainment</Link>
+                <Link to="/newsPage/politics" className="nav-link">Politics</Link>
+                <Link to="/newsPage/food" className="nav-link">Food</Link>
+                <Link to="/newsPage/business" className="nav-link">Business</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    </div>
     </>
   )
 }
