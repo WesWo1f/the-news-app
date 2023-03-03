c294f7b6517ebbc15ed015fb4293c59844035f70
Wes <claude.w.fountain@gmail.com>
2023-03-02 19:19:18 -0500

    removed lazyloading and back to react ver 18

--- src/components/DisplayNews.js
+++ src/components/DisplayNews.js
@@ -1,37 +1,36 @@
 import React from 'react';
-import LazyLoad from 'react-lazyload';
 import "../styles/displayNews.css"
 
 export default function DisplayNews({searchId, newsBlocks}) {
-  if (typeof(newsBlocks) !== 'undefined') {
+
+
+  if(typeof(newsBlocks) !== 'undefined'){
+    console.log(newsBlocks)
     return (
       <>
         <div className='news-cards-container'>
-          <div className='search-id'>{searchId}</div>
+          <div className='search-id'>{searchId}</div> 
           <ul>
             {newsBlocks.pageOne.fetchResult.data.map((article, index) => {
               return (
                 <li key={article.url}>
                   <hr className="solid"></hr>
                   <div className='article-container'>
-                    <LazyLoad height={170} once>
-                      <img className='article-image' src={article.image_url} alt={article.title} />
-                    </LazyLoad>
+                    <img  className='article-image' src={article.image_url} />
                     <div className='article-info'>
                       <div className='article-source'>{article.source}</div>
                       <div className='article-title'>{article.title}</div>
                       <div className='article-description'>{article.description}</div>
                     </div>
                   </div>
-                </li>
+                </li> 
               );
             })}
-          </ul>
-        </div>
+          </ul> 
+        </div> 
       </>
-    );
-  }
-}
+    )
+  }}
 
 
 
