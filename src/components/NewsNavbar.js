654aac86442726d919b44a1e19f6546bf41b33a1
Wes <claude.w.fountain@gmail.com>
2023-03-13 09:07:38 -0400

    styles on navbar collapsed and refactoring

--- src/components/NewsNavbar.js
+++ src/components/NewsNavbar.js
@@ -12,16 +12,16 @@ export default function NewsNavbar(setSearchId) {
   return (
     <>
     <div className='navbar-container'>
-      <div className='brand-and-links'>
+      <div className='brand-and-links' >
       <img className='logo' src={logo} alt="Logo" />
         <Navbar.Brand><Link className='nav-brand' to="/"></Link></Navbar.Brand>
       </div>
       <div className='brand-and-links'>
-        <Navbar   expand="lg">
+        <Navbar expand="lg" variant="dark" >
           <Container>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse  id="basic-navbar-nav">
-              <Nav className="me-auto">
+            <Navbar.Collapse >
+              <Nav className="me-auto" >
                 <Link to="/newsPage/general" className="nav-link">General</Link>
                 <Link to="/newsPage/science" className="nav-link">Science</Link>
                 <Link to="/newsPage/tech" className="nav-link">Tech</Link>
