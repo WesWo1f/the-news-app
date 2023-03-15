13ec38eba98b60b18a961562f63055a27633cdfc
Wes <claude.w.fountain@gmail.com>
2023-03-15 15:59:56 -0400

    added new search feature to make request to server

--- src/components/NewsFetch.js
+++ src/components/NewsFetch.js
@@ -1,19 +1,24 @@
 import { useEffect } from 'react'
 
 export default function NewsFetch({searchId, crawlData, setCrawlData, setNewsBlocks}) {
-
-  async function fetchData(url, id, page, newsUuid) {
+  
+  async function fetchData(url, id = null, page, searchBar) {
     let data = {}
     try {
+      const headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      }
+  
+      // Only include the id parameter if it's not null
+      const body = id ? JSON.stringify({category: id, page: page, searchBar: searchBar}) : JSON.stringify({page: page, searchBar: searchBar})
+  
       const response = await fetch(url, {
         method: 'POST', // or 'PUT'
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({category: id, page: page, newsUuid: newsUuid})
-    })
-    data = await response.json()
+        headers: headers,
+        body: body
+      })
+      data = await response.json()
     } catch (error) {
       //console.log(error)
     }
@@ -22,14 +27,22 @@ export default function NewsFetch({searchId, crawlData, setCrawlData, setNewsBlo
 
   useEffect(()=>{
     console.log("this is searchId"+searchId)
-    async function callingTest(){
-      console.log("called test server!")
-      let pageOne = await fetchData("https://nameless-cliffs-00097.herokuapp.com/newsendpoint", searchId, 1)
-      const newData = {pageOne} 
-      setNewsBlocks(newData)
+    async function navbarCategorySearch(){
+      if (['general', 'science', 'tech', 'sports','travel','entertainment','politics','business'].includes(searchId)) {
+        //let pageOne = await fetchData("https://nameless-cliffs-00097.herokuapp.com/newsendpoint", searchId, 1)
+        let pageOne = await fetchData("http://localhost:8000/newsendpoint", searchId, 1)
+        const newData = {pageOne} 
+        setNewsBlocks(newData)
+      }
+      else{
+        //let pageOne = await fetchData("https://nameless-cliffs-00097.herokuapp.com/newsendpoint", null, 1, searchId)
+        let pageOne = await fetchData("http://localhost:8000/newsendpoint", null, 1, searchId)
+        const newData = {pageOne} 
+        setNewsBlocks(newData)
+      }
     }
     if(typeof(searchId) !== 'undefined'){
-      callingTest()
+      navbarCategorySearch()
     }
   },[searchId])
 
