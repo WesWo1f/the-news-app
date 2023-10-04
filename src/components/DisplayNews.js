71b0c722c9b9027da002a97e76be662e16cc062d
Wes <claude.w.fountain@gmail.com>
2023-10-04 15:32:01 -0400

    slight refactor of DisplayNews.js

--- src/components/DisplayNews.js
+++ src/components/DisplayNews.js
@@ -1,68 +1,75 @@
-import React from 'react';
-import { useEffect } from 'react';
-import { useState } from 'react';
-import "../styles/displayNews.css"
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import "../styles/displayNews.css";
 
-export default function DisplayNews({searchId, newsBlocks}) {
+export default function DisplayNews({ searchId, newsBlocks }) {
   const [newslist, setNewsList] = useState();
-
+  
   useEffect(() => {
-    if(newsBlocks !== undefined && newsBlocks !== null){
-      let newsDataArray = newsBlocks.pageOne.fetchResult.data.filter((user) =>
-      !(
-        user.image_url.includes("default") ||
-        user.image_url.includes("logo-bg") ||
-        user.image_url.includes("movers") ||
-        user.image_url.includes("logo") 
-      )
-    );
-      setNewsList(newsDataArray)
+    if (newsBlocks !== undefined && newsBlocks !== null) {
+      let newsDataArray = newsBlocks.pageOne.fetchResult.data.filter(
+        (user) =>
+          !(
+            user.image_url.includes("default") ||
+            user.image_url.includes("logo-bg") ||
+            user.image_url.includes("movers") ||
+            user.image_url.includes("logo")
+          )
+      );
+      setNewsList(newsDataArray);
     }
-  }, [newsBlocks])
+  }, [newsBlocks]);
 
-  function deleteBadImageStory(uuidNumber){
-    let filteredList = newslist.filter(number => number.uuid !== uuidNumber.toString());
-    setNewsList(filteredList)
+  function deleteBadImageStory(uuidNumber) {
+    let filteredList = newslist.filter((number) => number.uuid !== uuidNumber.toString());
+    setNewsList(filteredList);
   }
 
-  if(typeof(newslist) !== 'undefined'){
+
+  function NewsArticles({newsData}) {
+    const { source, image_url, url, title, description, uuid } = newsData;
+        return (
+          <li key={url}>
+            <hr className="solid"></hr>
+            <a
+              href={url}
+              className="article-link"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className="article-container">
+                  <img
+                    alt=""
+                    className="article-image"
+                    src={image_url}
+                    onError={() => {
+                     deleteBadImageStory(uuid);
+                    }}
+                  />
+                <div className="article-info">
+                  <div className="article-source">{source}</div>
+                  <div className="article-title">{title}</div>
+                  <div className="article-description">{description}</div>
+                </div>
+              </div>
+            </a>
+          </li>
+  )}
+
+
+  if (typeof newslist !== "undefined") {
     return (
       <>
-        <div className='news-cards-container'>
-          <div className='search-id'>{searchId}</div> 
+        <div className="news-cards-container">
+          <div className="search-id">{searchId}</div>
           <ul>
-            {newslist.map((article, index) => {
-              
-              const {source, image_url, url, title, description, uuid} = article
-              return (
-                <li key={url}>
-                  <hr className="solid"></hr>
-                  <a href={url} className='article-link' target="_blank" rel="noopener noreferrer">
-                  <div className='article-container'>
-                            {image_url && (
-                        <img
-                          alt=''
-                          className='article-image'
-                          src={image_url}
-                          onError={() => {deleteBadImageStory(uuid)}}
-                        />
-                      )}
-                      <div className='article-info'>
-                      <div className='article-source'>{source}</div>
-                      <div className='article-title'>{title}</div>
-                      <div className='article-description'>{description}</div>
-                    </div>
-                  </div>
-                  </a>
-                </li> 
-              );
-            })}
-          </ul> 
-        </div> 
+            {newslist.map((news) => (
+              <NewsArticles newsData={news} key={news.uuid} />
+            ))}
+          </ul>
+        </div>
       </>
-    )
+    );
   }
 }
-
-
-
